d7857f1c2099b94f9ee054edf938f10276930031
Vladislav Podolskii <vladislav.podolskii@gmail.com>
2017-10-31 17:21:58 +0200

    My first project

---
+++ p0180constantlayoutintro/src/main/java/com/example/constantlayoutintro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.constantlayoutintro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.google_plus_screen);
+    }
+}
